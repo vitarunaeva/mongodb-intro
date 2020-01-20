c381317523d5edaef1667bc9bcae7fed3baeccc4
Violetta Runaeva <runaevavioletta@gmail.com>
2020-01-20 03:28:13 +0300

    Revert "add conn.js"
    This reverts commit 2a7f1bc8

--- controllers/findAllControllers.js
+++
@@ -1,9 +0,0 @@
-
-const {User} = require('../conn');
-
-module.exports = async r => {
-    const result = await User.find();
-    result ? r.res.send(result) : r.res.send('No such user');
-};
-
-//localhost:3000/users/list
--- controllers/findUserController.js
+++
@@ -1,9 +0,0 @@
-const {User} = require('../conn');
-
-module.exports = async r => {
-    const { login } = r.query;
-    const result = await User.findOne({login });
-    result ? r.res.send(result) : r.res.send('Do not exist');
-};
-//localhost:3000/users/?login=name
-//curl http://localhost:3000/users/?login=name
